3a1a621a3dbf33e87049ce52a323f13674241681
Arnoways <34322845+Arnoways@users.noreply.github.com>
2017-12-12 17:21:19 +0100

    Create PS_send_mail.ps1
    Send an email with powershell.

---
+++ PS_send_mail.ps1
@@ -0,0 +1,13 @@
+## in order to create password file, run: read-host -assecurestring | convertfrom-securestring | out-file C:\Path\to_the_file\containingpassword.txt                    
+#Note: for gmail users, you have to change some security settings into your account
+
+$username = "username"
+$password = cat C:\Path\to_the_file\containingpassword.txt | ConvertTo-SecureString
+$cred = new-object -typename System.Management.Automation.PSCredential -ArgumentList $username, $password
+$from = "sender email address"
+$to = "recipient email address"
+$subject = "Whatever you wwant"
+$Body = "Body of the email the script will send"
+$smtpserver = "smtp server"
+
+Send-MailMessage -from $from -to $to -subject $subject -body $Body -SmtpServer $smtpserver -Port 587 -UseSsl -Credential $cred 
