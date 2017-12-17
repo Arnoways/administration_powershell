55552d53a7a5241c320cb0220844705ebf36ba71
Arnoways <34322845+Arnoways@users.noreply.github.com>
2017-12-17 17:46:43 +0100

    Import Users from CSV in AD

---
+++ Import_User_FromCSV.ps1
@@ -0,0 +1,45 @@
+﻿Import-Module ActiveDirectory 
+
+#displaying window to fetch csv file
+Function Select-FolderDialog
+{
+    param([string]$Description="Select Folder",[string]$RootFolder="Desktop")
+
+ [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null     
+
+   $objForm = New-Object System.Windows.Forms.OpenFileDialog
+        $objForm.InitialDirectory = $RootFolder
+        #filters files to find csv ones only
+        $objForm.filter = "CSV (*.csv)| *.csv"
+        $objForm.ShowDialog() | Out-Null
+        $objForm.FileName
+}
+
+$file = Select-FolderDialog # the variable contains user folder selection
+
+
+
+$Users = Import-Csv -Delimiter ";" -Path $file -Encoding UTF8
+$OU = ""  #OU Path. (OU=Users,DC=domain,DC=local)
+
+
+
+foreach ($User in $Users)  
+{  
+    $SAMAccountName=$User.SAMAccountName
+    $Surname=$User.Surname
+    $GivenName=$User.GivenName
+    $email=$User.emailaddress
+    $script=$SAMAccountName+".bat"
+    $Name=$GivenName + " " + $Surname.ToUpper()
+    $upn=($SAMAccountName + "@" + $env:USERDNSDOMAIN).ToLower()
+    [array]$groups=$User.groupe -split ","
+    $pass=$User.password 
+    New-ADUser -AccountExpirationDate $null -Enabled $true -EmailAddress $email -Name $Name -UserPrincipalName $upn -SamAccountName $SAMAccountName -ScriptPath $script -GivenName $GivenName -Surname $Surname -Path $OU -ChangePasswordAtLogon $true
+    foreach ($group in $groups)
+    {
+    Add-ADGroupMember -Identity $group -Member $SAMAccountName
+    }
+} 
+Write-Host "User import done" -ForegroundColor Yellow
+dsa.msc
